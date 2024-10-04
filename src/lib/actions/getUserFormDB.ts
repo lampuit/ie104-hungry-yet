cc67c44f9b460685474b2d72bb83fb7374b69b27
dunnokiet <tianhaivn@gmail.com>
2024-10-05 05:21:36 +0700

    refactor: clean up for 'develop' branch

--- src/lib/actions/getUserFormDB.ts
+++
@@ -1,8 +0,0 @@
-
-'use server'
-import { db } from "@/db/db";
-import { Users } from "@/db/schema";
-export default async function getUserFromDB (){
-    const result = await db.select().from(Users);
-    console.log(result);
-}
