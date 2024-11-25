3c7a21b27572e1ce76f885c232a48b61d397db0a
Lam Pham <22520743@gm.uit.edu.vn>
2024-11-25 11:51:10 +0700

    refactor: update shopping cart schema references for consistency

--- src/lib/actions/shopping-cart.ts
+++ src/lib/actions/shopping-cart.ts
@@ -2,13 +2,12 @@
 
 import { db } from "@/drizzle/db";
 import {
-  inserShoppingCartSchema,
-  shoppingCart,
+  inserCartSchema,
+  carts,
 } from "@/drizzle/schema/project";
 import { eq, and } from "drizzle-orm";
 
-const CreateShoppingCart = inserShoppingCartSchema.omit({
-  id: true,
+const CreateShoppingCart = inserCartSchema.omit({
   createdAt: true,
   updatedAt: true,
 });
@@ -19,28 +18,32 @@ export async function createShoppingCart(formData: FormData) {
     productId: formData.get("productId"),
     quantity: Number(formData.get("quantity")),
   });
-    // If the item does not exist, insert a new item
-    const response = await db.insert(shoppingCart).values(data);
-    console.log("response",response);
+  // If the item does not exist, insert a new item
+  const response = await db.insert(carts).values(data);
+  console.log("response", response);
 
 }
 
 export async function updateShoppingCart(formData: FormData) {
   await db
-    .update(shoppingCart)
+    .update(carts)
     .set({ quantity: Number(formData.get("quantity")) })
     .where(
       and(
-        eq(shoppingCart.userId, formData.get("userId") as string),
-        eq(shoppingCart.productId, formData.get("productId") as string),
+        eq(carts.userId, formData.get("userId") as string),
+        eq(carts.productId, formData.get("productId") as string),
       ),
     );
 }
 
-export async function deleteShoppingCart(id: string) {
-  try{
-  await db.delete(shoppingCart).where(eq(shoppingCart.id, id));
+export async function deletecarts(productId: string, userId: string) {
+  try {
+    await db.delete(carts).where(
+      and(
+        eq(carts.productId, productId), eq(carts.userId, userId)
+      )
+    )
   } catch (error) {
-    console.error('Error deleting shopping cart',error);
+    console.error('Error deleting shopping cart', error);
   }
 }
