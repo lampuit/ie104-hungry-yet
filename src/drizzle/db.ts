048fb77dcf8919a32762ae88c4606ab4af3a5f51
dunnokiet <tianhaivn@gmail.com>
2024-11-28 16:59:38 +0700

    feat: add websocket for node.js env

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,10 +2,13 @@ import * as auth from "@/drizzle/schema/auth";
 import * as project from "@/drizzle/schema/project";
 
 import { config } from "dotenv";
-import { Pool } from "@neondatabase/serverless";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
 
 config({ path: ".env" });
+
+neonConfig.webSocketConstructor = ws;
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle({ client: pool, schema: { ...auth, ...project } });
