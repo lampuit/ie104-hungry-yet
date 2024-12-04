1b8f141f07de0e356bec22f74df847570619d973
Thanhliem Do <22520751@gm.uit.edu.vn>
2024-12-04 15:26:18 +0700

    fix: adjust z-index and visibility for product images on Exhibition page

--- src/app/exhibition/page.tsx
+++ src/app/exhibition/page.tsx
@@ -18,6 +18,7 @@ export default function ExhibitionPage() {
                     alt="thumbnail"
                     layout="fill"
                     objectFit='cover'
+                    className='absolute -z-10'
                 >
                 </Image>
             </div>
@@ -25,15 +26,15 @@ export default function ExhibitionPage() {
             <div className='flex flex-wrap justify-center items-center gap-3 w-4/5'>
                 {products?.map((product, index) => {
                     return (
-                        <div key={index} className='relative w-[300px] h-[200px] overflow-clip bg-black'>
+                        <div key={index} className='relative w-[300px] h-[200px] overflow-clip group'>
                             <Image src={product.imageUrl}
                                 alt={product.name}
                                 layout="fill"
                                 objectFit='cover'
-                                className='absolute transition transform duration-300 ease-in-out hover:scale-125 hover:opacity-50 z-0'
+                                className='absolute transition transform duration-300 ease-in-out hover:scale-125 hover:opacity-50 -z-10'
                             >
                             </Image>
-                            <div className='flex flex-col justify-center items-center h-full text-white z-10'>
+                            <div className='hidden group-hover:flex flex-col justify-center items-center h-full text-white z-10'>
                                 <h1 className='text-xl font-semibold'>{product.name}</h1>
                                 <Button>Xem chi tiết</Button>
                             </div>
