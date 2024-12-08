4247d3b16bd185932ae264a814ef25589af1f495
Lam Pham <22520743@gm.uit.edu.vn>
2024-12-08 21:15:18 +0700

    refactor: update messages prop type to use Message[] for better type safety

--- src/components/chatbot/chat-message.tsx
+++ src/components/chatbot/chat-message.tsx
@@ -1,13 +1,14 @@
 
 import { Button } from "@/components/ui/button";
 import { useAutoScroll } from "@/hooks/use-auto-scroll";
+import { Message } from "ai";
 import { ArrowDown } from "lucide-react";
 
 export function ChatMessages({
   messages,
   children,
 }: React.PropsWithChildren<{
-  messages: any;
+  messages: Message[];
 }>) {
   const {
     containerRef,
