51dac1388d5c90ce9640829a3c11f72bb6f5f954
dunnokiet <tianhaivn@gmail.com>
2024-12-13 11:38:49 +0700

    chore: add dialog chatbot in menu, homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,40 +18,49 @@ import { motion } from "framer-motion";
 import { MobileHero } from "@/components/home/mobile-hero";
 import { MobileBenefit } from "@/components/home/mobile-benefit";
 import { Charm } from "next/font/google";
+import Chatbot from "@/components/chatbot/chat-bot";
 const charm = Charm({
   subsets: ["vietnamese"],
   weight: ["400", "700"],
 });
 
 export default function Homepage() {
-
   return (
     <main>
       {/*Herosection ở màn hình Desktop*/}
-      <header className="hidden relative sm:flex flex-col justify-between items-center h-[calc(100vh-80px)] w-screen overflow-hidden bg-black" >
-        <div className="absolute right-full h-[calc(100vh-80px)] z-0">
+      <header className="relative hidden h-[calc(100vh-80px)] w-screen flex-col items-center justify-between overflow-hidden bg-black sm:flex">
+        <div className="absolute right-full z-0 h-[calc(100vh-80px)]">
           <ImgBg />
         </div>
-        <div className="absolute right-full h-[calc(100vh-80px)] z-10">
+        <div className="absolute right-full z-10 h-[calc(100vh-80px)]">
           <ImgBgHover />
         </div>
-        <div className="absolute w-full h-[calc(100vh-80px)] bg-gradient-radial from-black to-transparent z-0"></div>
-        <motion.div className="relative top-1/3 flex flex-col justify-between items-center min-h-52 max-h-64 text-white z-10 py-4 gap-8"
+        <div className="absolute z-0 h-[calc(100vh-80px)] w-full bg-gradient-radial from-black to-transparent"></div>
+        <motion.div
+          className="relative top-1/3 z-10 flex max-h-64 min-h-52 flex-col items-center justify-between gap-8 py-4 text-white"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 2.5, ease: "easeOut" }}>
-          <h1 className={`${charm.className} text-7xl md:text-8xl text-center`}>Hungry Yet?</h1>
-          <p className="text-xl md:text-2xl text-center">Chọn hương vị, nhận yêu thương - chỉ từ một cú CLICK!</p>
-          <div className="flex justify-around min-w-72 gap-x-3">
-            <Button asChild className="bg-amber-500 rounded-3xl hover:bg-red-500">
+          transition={{ duration: 2.5, ease: "easeOut" }}
+        >
+          <h1 className={`${charm.className} text-center text-7xl md:text-8xl`}>
+            Hungry Yet?
+          </h1>
+          <p className="text-center text-xl md:text-2xl">
+            Chọn hương vị, nhận yêu thương - chỉ từ một cú CLICK!
+          </p>
+          <div className="flex min-w-72 justify-around gap-x-3">
+            <Button
+              asChild
+              className="rounded-3xl bg-amber-500 hover:bg-red-500"
+            >
               <Link href={"/menu/cart"}>Đặt hàng ngay</Link>
             </Button>
-            <Button asChild className="bg-black rounded-3xl hover:bg-red-500">
+            <Button asChild className="rounded-3xl bg-black hover:bg-red-500">
               <Link href={"/menu"}>Xem thực đơn</Link>
             </Button>
           </div>
         </motion.div>
-        <div className="relative center sm:end-1/4 md:end-1/3 z-10">
+        <div className="center relative z-10 sm:end-1/4 md:end-1/3">
           <TimeAndAddress />
         </div>
       </header>
@@ -59,17 +68,21 @@ export default function Homepage() {
       {/*Herosection ở màn hình Mobile*/}
       <MobileHero />
 
-      <section className="hidden sm:inline-block my-16 overflow-clip">
+      <section className="my-16 hidden overflow-clip sm:inline-block">
         <Categories />
       </section>
 
-      <section className="flex flex-col items-center mt-10 lg:mb-20">
-      <div className="flex flex-col justify-center items-center gap-y-4">
-        <p className={`${charm.className} inline-block sm:hidden italic font-semibold text-4xl text-center`}>Dịch vụ cung cấp</p>
-        <div className="inline-bock sm:hidden">
-        <HorizontalLine />
+      <section className="mt-10 flex flex-col items-center lg:mb-20">
+        <div className="flex flex-col items-center justify-center gap-y-4">
+          <p
+            className={`${charm.className} inline-block text-center text-4xl font-semibold italic sm:hidden`}
+          >
+            Dịch vụ cung cấp
+          </p>
+          <div className="inline-bock sm:hidden">
+            <HorizontalLine />
           </div>
-      </div>
+        </div>
         <Benefit />
         <MobileBenefit />
       </section>
@@ -86,44 +99,61 @@ export default function Homepage() {
         <OrderOnline />
       </section>
 
-      <section className="flex flex-col items-center mx-5 sm:mx-10 mb-12 sm:px-10 space-y-8">
-        <div className="flex flex-col sm:flex-row justify-between items-center gap-4 sm:gap-0 w-full max-w-screen-2xl">
+      <section className="mx-5 mb-12 flex flex-col items-center space-y-8 sm:mx-10 sm:px-10">
+        <div className="flex w-full max-w-screen-2xl flex-col items-center justify-between gap-4 sm:flex-row sm:gap-0">
           <div className="flex flex-col gap-y-4">
-            <h2 className={`${charm.className} italic font-semibold text-4xl text-center sm:text-5xl`}>Best Seller</h2>
+            <h2
+              className={`${charm.className} text-center text-4xl font-semibold italic sm:text-5xl`}
+            >
+              Best Seller
+            </h2>
             <HorizontalLine />
           </div>
-          <Button asChild className="hidden sm:flex items-center bg-black hover:bg-red-500 rounded-3xl">
+          <Button
+            asChild
+            className="hidden items-center rounded-3xl bg-black hover:bg-red-500 sm:flex"
+          >
             <Link href={"/exhibition"}>Xem blog</Link>
           </Button>
         </div>
-        <div className="flex justify-center w-full">
+        <div className="flex w-full justify-center">
           <Exhibition />
         </div>
-        <Button asChild className="sm:hidden bg-black hover:bg-red-500 rounded-3xl">
-            <Link href={"/exhibition"}>Xem blog</Link>
-          </Button>
+        <Button
+          asChild
+          className="rounded-3xl bg-black hover:bg-red-500 sm:hidden"
+        >
+          <Link href={"/exhibition"}>Xem blog</Link>
+        </Button>
       </section>
 
-      <section className="flex flex-col items-center mx-10 md:px-10 px-6 mb-16 space-y-8">
-        <div className="flex flex-col justify-between items-center sm:items-start gap-4 w-full max-w-screen-2xl">
-          <h2 className={`${charm.className} italic font-semibold text-4xl text-center sm:text-5xl`}>Nhận xét & đánh giá</h2>
+      <section className="mx-10 mb-16 flex flex-col items-center space-y-8 px-6 md:px-10">
+        <div className="flex w-full max-w-screen-2xl flex-col items-center justify-between gap-4 sm:items-start">
+          <h2
+            className={`${charm.className} text-center text-4xl font-semibold italic sm:text-5xl`}
+          >
+            Nhận xét & đánh giá
+          </h2>
           <HorizontalLine />
         </div>
         <Testimonials />
-
       </section>
 
-      <section className="mx-10 px-10 mb-16 flex flex-col lg:flex-row justify-center items-center lg:items-start gap-10 lg:gap-40">
+      <section className="mx-10 mb-16 flex flex-col items-center justify-center gap-10 px-10 lg:flex-row lg:items-start lg:gap-40">
         <div className="flex flex-col justify-center gap-4">
-          <h2 className={`${charm.className} italic font-semibold text-4xl text-center lg:text-start sm:text-5xl`}>FAQs</h2>
+          <h2
+            className={`${charm.className} text-center text-4xl font-semibold italic sm:text-5xl lg:text-start`}
+          >
+            FAQs
+          </h2>
           <HorizontalLine />
         </div>
         <ScrollArea className="h-[400px]">
           <FAQ />
         </ScrollArea>
       </section>
+
+      <Chatbot />
     </main>
   );
 }
-
-
